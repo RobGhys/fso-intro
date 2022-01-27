d87505c083d0638a9f0bec8d29a5f2f5e632c8c8
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 09:24:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    console.log('Hello from component')
+    const now = new Date();
+    const a = 10;
+    const b = 20;
+
+    return (
+        <div>
+            <p>Hello world</p>
+            <p>
+                {a} plus {b} = {a + b}
+            </p>
+        </div>
+    )
 }
 
 export default App;
