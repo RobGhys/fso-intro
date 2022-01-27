ae14697b3d4c9f36b21b96b916d10f529f493c07
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 09:31:48 +0100

    second commit end of intro to react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}, you are {props.age} y/o</p>
+        </div>
+    )
+}
+
 const App = () => {
     console.log('Hello from component')
     const now = new Date();
     const a = 10;
     const b = 20;
+    const name = "Marlon";
 
     return (
         <div>
-            <p>Hello world</p>
+            <p>Greetings</p>
             <p>
                 {a} plus {b} = {a + b}
             </p>
+            <Hello name={"Robin"} age={20}/>
+            <Hello name={name} age={a + b}/>
         </div>
     )
 }
