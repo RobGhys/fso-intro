9f0ff5dbc142eb80b3ef7b94a9bfe620d54a878a
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 11:19:50 +0100

    implemented ex 1.3

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ const NbEx = (props) => {
 const Content = (props) => {
     return (
         <div>
-            <Part title={props.part1} exercise={props.exercises1}/>
-            <Part title={props.part2} exercise={props.exercises2}/>
-            <Part title={props.part3} exercise={props.exercises3}/>
+            <Part title={props.part1.name} exercise={props.part1.exercises}/>
+            <Part title={props.part2.name} exercise={props.part2.exercises}/>
+            <Part title={props.part3.name} exercise={props.part3.exercises}/>
         </div>
     )
 }
@@ -43,12 +43,18 @@ const App = () => {
     const b = 20;
     const name = "Marlon";
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
+    }
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
 
     return (
         <div>
@@ -60,8 +66,8 @@ const App = () => {
             <Hello name={"Robin"} age={20}/>
             <Hello name={name} age={a + b}/>
             <Course course={course}/>
-            <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-            <NbEx nbr={exercises1 + exercises2 + exercises3}/>
+            <Content part1={part1} part2={part2} part3={part3}/>
+            <NbEx nbr={part1.exercises + part2.exercises + part3.exercises}/>
         </div>
     )
 }
