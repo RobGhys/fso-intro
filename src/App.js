a619fed42b0b3d2848dbd4044abc90f8f9b21323
Robin Ghyselinck <robin.ghyselinck2@kbc.be>
2022-01-27 09:37:45 +0100

    completed exercice 1.1-1.2

--- src/App.js
+++ src/App.js
@@ -8,12 +8,37 @@ const Hello = (props) => {
     )
 }
 
+const Course = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>{props.title} {props.exercise}</p>
+    )
+}
+
+const NbEx = (props) => {
+    return (
+        <p>Number of exercises {props.nbr}</p>
+    )
+}
+
 const App = () => {
     console.log('Hello from component')
     const now = new Date();
     const a = 10;
     const b = 20;
     const name = "Marlon";
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
@@ -23,6 +48,11 @@ const App = () => {
             </p>
             <Hello name={"Robin"} age={20}/>
             <Hello name={name} age={a + b}/>
+            <Course course={course}/>
+            <Part title={part1} exercise={exercises1}/>
+            <Part title={part2} exercise={exercises2}/>
+            <Part title={part3} exercise={exercises3}/>
+            <NbEx nbr={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
